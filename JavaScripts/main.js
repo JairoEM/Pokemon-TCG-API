1615912489176786fc864a11d8308ac17f29f90c
Jairo <loseloth@gmail.com>
2019-01-07 20:21:01 +0100

    PokeAPI

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,13 +1,47 @@
 $("document").ready( () => {
+    var $cards = $("section#pokemonCards");
+    var $win = $(window);
+    var page = 1;
 
+    // INITIAL PETITION
     $.ajax({
         method: "GET",
-        url: "https://api.pokemontcg.io/v1/cards"
+        url: "https://api.pokemontcg.io/v1/cards?page="+ page +"&pageSize=12"
     })
-    .done(function(answer){
-        console.log(answer);
+    
+    .done(function(result){          
+        for(let i = 0; i < result.length; i++){
+            $cards.append("<div class='pokemonCard'></div>");
+            var $card = $("div.pokemonCard:last-child");
+            // TAGS
+            $card.append("<h3>"+ result.cards[i].name +"</h3>");
+            $card.append("<img src="+ result.cards[i].imageUrl +">");
+        }
+        page++;
     })
+
     .fail(function(){
-        console.log("Something when wrong");
+        console.error("API NOT WORKING");
+        alert("API NOT WORKING");
+    });
+
+    $win.scroll( function() {
+        if ( $(document).height() - $win.height() == $win.scrollTop()) {
+            $.ajax({
+                method: "GET",
+                url: "https://api.pokemontcg.io/v1/cards?page="+ page +"&pageSize=12",
+
+                success: function(result) {     
+                    for(let i = 0; i < result.length; i++){
+                        $cards.append("<div class='pokemonCard'></div>");
+                        var $card = $("div.pokemonCard:last-child");
+                        // TAGS
+                        $card.append("<h3>"+ result.cards[i].name +"</h3>");
+                        $card.append("<img src="+ result.cards[i].imageUrl +">");
+                    }
+                    page++;
+                }
+            });
+        }
     });
 });
